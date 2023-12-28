b5f63f76afffda51302cd76d029fcd9aa6cbf558
gjacquet-van <60664665+gjacquet-van@users.noreply.github.com>
2023-12-28 12:48:45 +0100

    prettier

--- GenForm-NPM/React/index.js
+++ GenForm-NPM/React/index.js
@@ -2,21 +2,21 @@ import React, { useEffect, useRef } from 'react'
 import GenForm from '@genform/core'
 
 function GenFormComponent({ elems, params }) {
-    const formRef = useRef(null)
+  const formRef = useRef(null)
 
-    useEffect(() => {
-        const generatedForm = GenForm.toForm(document, {
-            elems: elems,
-            params: params
-        })
+  useEffect(() => {
+    const generatedForm = GenForm.toForm(document, {
+      elems: elems,
+      params: params
+    })
 
-        if (formRef.current) {
-            formRef.current.innerHTML = ''
-            formRef.current.appendChild(generatedForm)
-        }
-    }, [elems, params])
+    if (formRef.current) {
+      formRef.current.innerHTML = ''
+      formRef.current.appendChild(generatedForm)
+    }
+  }, [elems, params])
 
-    return React.createElement('div', { ref: formRef })
+  return React.createElement('div', { ref: formRef })
 }
 
 export default GenFormComponent
