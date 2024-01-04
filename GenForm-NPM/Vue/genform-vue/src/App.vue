aebfe684b1f9829052d1c8b0d2c061e87ccd647b
lisa-rl <lisa.raoul@efrei.net>
2024-01-04 15:07:22 +0100

    Univers test vue js en cours

---
+++ GenForm-NPM/Vue/genform-vue/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <GenFormComponent :elems="customElems" :params="customParams" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import GenFormComponent from './components/GenFormComponent.vue';
+
+export default {
+  components: {
+    GenFormComponent,
+  },
+  setup() {
+    const customElems = ref([
+      {
+        type: 'text',
+        name: 'name',
+        placeholder: 'Name'
+      },
+      {
+        type: 'email',
+        name: 'email',
+        placeholder: 'Email'
+      },
+      {
+        type: 'password',
+        name: 'password',
+        placeholder: 'Password'
+      },
+      {
+        type: 'submit',
+        value: 'Submit'
+      },
+      {
+        type: 'reset',
+        value: 'Reset'
+      }
+    ]);
+
+    const customParams = ref({
+      action: '/register',
+      method: 'POST',
+    });
+
+    return { customElems, customParams };
+  }
+};
+
+</script>
