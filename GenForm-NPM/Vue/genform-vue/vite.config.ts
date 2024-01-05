f29da6b59c5fa566787dfe9d975a367752f4cb2b
lisa-rl <lisa.raoul@efrei.net>
2024-01-05 10:09:49 +0100

    Refacto 2 package vue

--- GenForm-NPM/Vue/genform-vue/vite.config.ts
+++ GenForm-NPM/Vue/genform-vue/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'GenFormVue',
+      fileName: "@genform/genform-vue",
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
