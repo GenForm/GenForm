d98101dd269fc6c0ba420cc9a72c52b8405da416
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-05 20:27:06 +0100

    Inclusion partiel de babel (non fonctionnelle)

--- GenForm-Tests/Vanilla/index.js
+++ GenForm-Tests/Vanilla/index.js
@@ -1,4 +1,7 @@
-import GenForm from 'https://unpkg.com/@genform/core@0.1.5/index.min.js'
+// UNPKG allow to retrieve a specific file from a node package (here a minified version of the core of GenForm)
+// import genform from 'https://unpkg.com/@genform/core@latest/index.min.js'
+import GenForm from 'https://unpkg.com/@genform/complete@0.0.6?module'
+
 // Because of this js file is called from index.html using a script tag with type="module", the function changeForm is not accessible from the global scope
 // To allow the function to be called whenever the button is clicked, we need to add an event listener to the button
 const submitButton = document.getElementById('submit')
