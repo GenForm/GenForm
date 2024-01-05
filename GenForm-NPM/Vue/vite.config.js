18899b0cdcf7ded2b42420c6c265d5c434b30103
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-05 22:29:44 +0100

    Prettier

--- GenForm-NPM/Vue/vite.config.js
+++ GenForm-NPM/Vue/vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/index.js'),
-      name: 'GenForm/Vue',
-      fileName: "GenFormComponent",
-    },
-    rollupOptions: {
-      external: ['vue', '@genform/core'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          '@genform/core': 'GenForm',
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/index.js'),
+            name: 'GenForm/Vue',
+            fileName: 'GenFormComponent'
         },
-      },
-    },
-  },
-});
+        rollupOptions: {
+            external: ['vue', '@genform/core'],
+            output: {
+                globals: {
+                    'vue': 'Vue',
+                    '@genform/core': 'GenForm'
+                }
+            }
+        }
+    }
+})
