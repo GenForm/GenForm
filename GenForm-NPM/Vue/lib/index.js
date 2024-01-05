f4457e906081f65e7027eb01b35bb24b3d81c42a
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-05 22:28:51 +0100

    Le package est fonctionnel
    L'erreur était due au index.js du package npm qui était config pour exporter un plugin et non un composant
    Si on voulait continuer a exporter un plugin, il fallait changer la façon dont on importe le package dans le test vue

--- GenForm-NPM/Vue/lib/index.js
+++ GenForm-NPM/Vue/lib/index.js
@@ -1,23 +1,3 @@
 import GenFormComponent from './components/GenFormComponent.vue';
 
-export function install(app) {
-  if (install.installed) return;
-
-  install.installed = true;
-  app.component('GenFormComponent', GenFormComponent);
-}
-
-const plugin = { install };
-
-// To auto-install when vue is found
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
 export default GenFormComponent
