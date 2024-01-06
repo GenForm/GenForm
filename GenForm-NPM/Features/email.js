edf94b83f6a239fa1ba194f245c3af6331b0ad18
natdoly <61319301+natdoly@users.noreply.github.com>
2024-01-06 15:33:07 +0100

    Fix using native attributes

--- GenForm-NPM/Features/email.js
+++ GenForm-NPM/Features/email.js
@@ -1,12 +1,17 @@
-function validateEmailInput(form) {
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+function validateEmailInput(form, json) {
+  const emailField = json.elems.find((elem) => elem.type === 'email')
   const emailInput = form.querySelector('input[type="email"]')
+  const emailRegex = new RegExp(emailField.pattern)
+
+  if (!emailField || !emailInput || !emailRegex) {
+    throw new Error('email field or input or regex not found')
+  }
 
   emailInput.addEventListener('input', function () {
-    if (this.value.includes('@') && this.value.includes('.')) {
-      if (!emailRegex.test(this.value)) {
-        throw new Error('invalid email')
-      }
+    if (!emailRegex.test(this.value)) {
+      throw new Error('Invalid email')
+    } else {
+      console.log('email valid format')
     }
   })
 }
