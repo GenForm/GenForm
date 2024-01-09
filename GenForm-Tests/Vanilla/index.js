c475b32e1a191a75004893f32fb9d0ccd2a0af7e
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-09 22:56:48 +0100

    ça marche toujours pas
    Le core n'est plus modifié et n'utilise plus babel.
    Le problème est décalé sur Complete et utilise Webpack et babel pour faire un bundle

--- GenForm-Tests/Vanilla/index.js
+++ GenForm-Tests/Vanilla/index.js
@@ -1,6 +1,7 @@
 // UNPKG allow to retrieve a specific file from a node package (here a minified version of the core of GenForm)
 // import genform from 'https://unpkg.com/@genform/core@latest/index.min.js'
-import GenForm from 'https://unpkg.com/@genform/complete@0.0.6?module'
+// import GenForm from 'https://unpkg.com/@genform/complete@0.0.6?module'
+// import GenForm from './index.bundle.js'
 
 // Because of this js file is called from index.html using a script tag with type="module", the function changeForm is not accessible from the global scope
 // To allow the function to be called whenever the button is clicked, we need to add an event listener to the button
@@ -11,7 +12,7 @@ function changeForm() {
   const json = document.getElementById('changeform').value
   const obj = JSON.parse(json)
   const formInHtml = document.getElementById('genform')
-  const form = GenForm.toForm(document, obj)
+  const form = e.toForm(document, obj)
 
   formInHtml.innerHTML = ''
   formInHtml.appendChild(form)
