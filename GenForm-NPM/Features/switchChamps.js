8c68ca73851b572d68962d80cb4803b7a4cbcc22
lisa-rl <lisa.raoul@efrei.net>
2024-01-16 22:09:18 +0100

    Fix prettier

--- GenForm-NPM/Features/switchChamps.js
+++ GenForm-NPM/Features/switchChamps.js
@@ -1,7 +1,7 @@
 // Function to check pattern validity
 export function checkPatternValidity(elem, onValid) {
-  return function(event) {
-    const inputElement = event.target 
+  return function (event) {
+    const inputElement = event.target
     const currentPattern = new RegExp(inputElement.pattern)
 
     const userInput = inputElement.value
@@ -11,10 +11,12 @@ export function checkPatternValidity(elem, onValid) {
     if (currentLength >= elem.maxChars) {
       // If a pattern is specified and not matched
       if (currentPattern && !currentPattern.test(userInput)) {
-        console.error(`Pattern not matched in ${elem.name}. Entered value: ${userInput}. Expected pattern: ${currentPattern}`)
+        console.error(
+          `Pattern not matched in ${elem.name}. Entered value: ${userInput}. Expected pattern: ${currentPattern}`
+        )
       } else {
         // Call the onValid callback when validation succeeds
-        onValid();
+        onValid()
       }
 
       // Truncate input value if it exceeds maxChars
@@ -24,8 +26,7 @@ export function checkPatternValidity(elem, onValid) {
 }
 
 // Function to check functionality with additional properties (autoSwitch)
-export function checkFeatures(form, autoSwitchFeatures) {  
-  
+export function checkFeatures(form, autoSwitchFeatures) {
   autoSwitchFeatures.forEach((feature) => {
     const inputName = feature.inputName
     const inputElement = form.querySelector(`[name="${inputName}"]`)
@@ -37,17 +38,21 @@ export function checkFeatures(form, autoSwitchFeatures) {
       }
 
       // Add an input event listener to check pattern validity
-      inputElement.addEventListener('input', 
-      checkPatternValidity(featureWithPattern, () => {return moveToNextInput(inputElement)}))
+      inputElement.addEventListener(
+        'input',
+        checkPatternValidity(featureWithPattern, () => {
+          return moveToNextInput(inputElement)
+        })
+      )
     }
   })
-}  
+}
 
 // Function to move to the next field
 export function moveToNextInput(currentInputElement) {
   // Retrieve the following element from the the form
   const nextInput = currentInputElement.nextElementSibling
-  
+
   // If the next element exists, focus on it
   if (nextInput) nextInput.focus()
 }
