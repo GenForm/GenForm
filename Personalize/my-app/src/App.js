ceebaa0dbbabc8b05764a1e4027e8f60ef840bb0
natdoly <61319301+natdoly@users.noreply.github.com>
2024-01-19 11:56:11 +0100

    first version t32

---
+++ Personalize/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ColorPicker from './components/picker';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPicker/>
+    </div>
+  );
+}
+
+export default App;
