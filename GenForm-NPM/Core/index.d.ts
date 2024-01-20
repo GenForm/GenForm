d01948b879c4ea1f5379589e463ebd80d61c28bd
lisa-rl <lisa.raoul@efrei.net>
2024-01-20 19:41:21 +0100

    Modify index.d.ts

--- GenForm-NPM/Core/index.d.ts
+++ GenForm-NPM/Core/index.d.ts
@@ -1,45 +1,36 @@
-/**
- * @typedef {Object} InputElem
- * @property {string} type - The type of the input element
- * @property {string} name - The name of the input element
- * @property {string} label - The label of the input element
- * @property {string} placeholder - The placeholder of the input element
- * @property {boolean} required - The required attribute of the input element
- */
+// Declarative file, read by the TypeScript compiler to determine the JS code type
 
-/**
- * @typedef {Object} FormParams
- * @property {string} action - The action of the form
- * @property {string} method - The method of the form
- */
+interface InputElem {
+  type: string;
+  name: string;
+  label: string;
+  placeholder: string;
+  required: boolean;
+}
 
-/**
- * @typedef {Object} FormObj
- * @property {InputElem[]} elems - The list of the inputs and their params
- * @property {FormParams} params - The params of the form
- */
+interface FormParams {
+  action: string;
+  method: string;
+}
 
-/**
- * This function creates a form in the DOM from an object listing the required inputs and their parameters
- * @param {Document} document - The required document object to create form's elements
- * @param {formObj} obj - The object containing the required inputs, their parameters, and the form parameters
- * @returns {HTMLFormElement} - The form element containing the specified elements
- */
+interface FormObj {
+  elems: InputElem[];
+  params: FormParams;
+}
+
 declare class GenForm {
   static validTypes: string[];
   static validAttributes: string[];
-
-  static toForm(document: Document, obj: FormObj): HTMLFormElement;
 }
 
-/**
- * @param {formObj} jsonObj - The JSON object to check for duplicate names
- * @throws {Error} - Throws an error if duplicate names are found
- */
-declare function nameIsDuplicate(jsonObj: FormObj): void;
+declare namespace GenForm {
+  /**
+   * This function creates a form in the DOM from an object listing the required inputs and their parameters
+   * @param {Document} document - The required document object to create form's elements
+   * @param {formObj} obj - The object containing the required inputs, their parameters, and the form parameters
+   * @returns {HTMLFormElement} - The form element containing the specified elements
+   */
+  function toForm(document: Document, obj: FormObj): HTMLFormElement;
+} 
 
-/**
- * @param {formObj} jsonObj - The input JSON to check for correctness
- * @throws {Error} - Throws an error if the JSON is incorrect
- */
-declare function jsonIsCorrect(inputJSON: FormObj): void;
+export default GenForm;
