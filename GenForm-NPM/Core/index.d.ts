fac87f9fd1f89b189c00d5b6eb4a7e9e3df313ae
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-20 19:56:50 +0100

    Prettier et commentaires

--- GenForm-NPM/Core/index.d.ts
+++ GenForm-NPM/Core/index.d.ts
@@ -1,36 +1,38 @@
 // Declarative file, read by the TypeScript compiler to determine the JS code type
 
 interface InputElem {
-  type: string;
-  name: string;
-  label: string;
-  placeholder: string;
-  required: boolean;
+  type: string
+  name: string
+  placeholder: string
+  required?: boolean
 }
 
 interface FormParams {
-  action: string;
-  method: string;
+  action: string
+  method: string
 }
 
 interface FormObj {
-  elems: InputElem[];
-  params: FormParams;
+  elems: InputElem[]
+  params: FormParams
 }
 
+/**
+ * This class will contain all the functions to generate and manipulate forms
+ */
 declare class GenForm {
-  static validTypes: string[];
-  static validAttributes: string[];
+  static validTypes: string[]
+  static validAttributes: string[]
 }
 
 declare namespace GenForm {
   /**
    * This function creates a form in the DOM from an object listing the required inputs and their parameters
    * @param {Document} document - The required document object to create form's elements
-   * @param {formObj} obj - The object containing the required inputs, their parameters, and the form parameters
+   * @param {FormObj} obj - The object containing the required inputs, their parameters, and the form parameters
    * @returns {HTMLFormElement} - The form element containing the specified elements
    */
-  function toForm(document: Document, obj: FormObj): HTMLFormElement;
-} 
+  function toForm(document: Document, obj: FormObj): HTMLFormElement
+}
 
-export default GenForm;
+export default GenForm
