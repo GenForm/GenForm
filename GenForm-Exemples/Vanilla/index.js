7e9ba8d7b78b6d9545f2b50bc2ace0bd6674ce04
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-30 14:38:05 +0100

    Update index.js

--- GenForm-Exemples/Vanilla/index.js
+++ GenForm-Exemples/Vanilla/index.js
@@ -1,5 +1,5 @@
 // UNPKG allow to retrieve a specific file from a node package (here a minified version of the core of GenForm)
-import genform from 'https://unpkg.com/@genform/core@0.1.34/index.min.js'
+import genform from 'https://unpkg.com/@genform/core@latest/index.min.js'
 
 // Because of this js file is called from index.html using a script tag with type="module", the function changeForm is not accessible from the global scope
 // To allow the function to be called whenever the button is clicked, we need to add an event listener to the button
